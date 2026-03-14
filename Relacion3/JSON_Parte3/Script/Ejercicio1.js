f6f8a38af0b16dc8a643ea682b6929eb43d2df01
Alexis Esteban Martín <aemgame4008@gmail.com>
2026-03-14 13:57:21 +0100

    Ultimos retoques ejercicio 1

--- Relacion3/JSON_Parte3/Script/Ejercicio1.js
+++ Relacion3/JSON_Parte3/Script/Ejercicio1.js
@@ -5,6 +5,8 @@ const datos = {
   contenido: "A las 7:00 pm en la puerta del teatro",
 };
 
+fetch("datos.json");
+
 document.getElementById("caja-para").textContent = "Para: " + datos.para;
 document.getElementById("caja-de").textContent = "De: " + datos.de;
 document.getElementById("caja-titulo").textContent = datos.titulo;
